d5082728e617af5c07c4383223a264b421c12f2d
latifalya <79078423+latifalya@users.noreply.github.com>
2022-06-02 23:41:51 +0700

    Update footer.blade.php

--- resources/views/includes/admin/footer.blade.php
+++ resources/views/includes/admin/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="sticky-footer bg-white d-print-none">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Petcare 2021</span>
+            <span>Copyright &copy; Petcare 2022</span>
         </div>
     </div>
 </footer>
