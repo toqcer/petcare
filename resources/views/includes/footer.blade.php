a57717232630a0bc39997bcd26d3bab75ac766d5
uloydev <47286547+uloydev@users.noreply.github.com>
2022-06-29 23:42:51 +0700

    [UPDATE] footer user

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,6 +3,11 @@
     <div class="container-fluid">
         <div class="row border-top justify-content-center align-items-center pt-4">
             <div class="col-auto text-gray-500 font-weight-light">
+                butuh bantuan? petcare@gmail.com
+            </div>
+        </div>
+        <div class="row justify-content-center align-items-center pt-2">
+            <div class="col-auto text-gray-500 font-weight-light">
                 2022 Copyright PetCare • All right reserved • By Alya Latifah Putri Achmad
             </div>
         </div>
